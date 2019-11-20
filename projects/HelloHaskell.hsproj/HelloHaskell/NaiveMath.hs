e78c023d2f1c3789b674e3cc031c576f26a78c0c
Gil Ho, Jang <jmxexpert@hotmail.com>
2019-11-21 00:18:20 +0900

    made codes short.

--- projects/HelloHaskell.hsproj/HelloHaskell/NaiveMath.hs
+++ projects/HelloHaskell.hsproj/HelloHaskell/NaiveMath.hs
@@ -5,8 +5,7 @@ module NaiveMath
 
 gcd' :: Integral a => a -> a -> a
 
-gcd' 0 n = n
+gcd' 0 n = abs n
 
 gcd' m n = let r = n `mod` m
-           in case r of 0 -> abs m
-                        _ -> gcd' r m
+           in gcd' r m
