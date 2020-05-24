c21d5ada78a47daf1a49add40419074971281bec
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-05-24 11:15:29 +0900

    added a solution for Problem 1-1.

---
+++ dynamic_programming/Untitled.hsproj/dp/lib/Problem_1_1.hs
@@ -0,0 +1,16 @@
+module Problem_1_1 () where
+
+-- by using a recursion
+fact :: (Num a, Eq a) => a -> a
+fact 1 = 1
+fact n = n * fact (n - 1)
+
+-- by using the 'product' function and list enumeration
+fact' :: (Num a, Enum a) => a -> a
+fact' n = product [1..n]
+
+-- by using the 'foldl1' function and list enumeration
+fact'' :: (Num a, Enum a) => a -> a
+fact'' n = foldl1 (*) [1..n]
+
+-- 
