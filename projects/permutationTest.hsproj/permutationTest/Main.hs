e8437efc54df42854d6743324c142fdd7568dcb9
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-05-27 17:50:01 +0900

    shortened some codes.

--- projects/permutationTest.hsproj/permutationTest/Main.hs
+++ projects/permutationTest.hsproj/permutationTest/Main.hs
@@ -2,14 +2,13 @@ import Data.List
 import Data.Monoid
 import Control.Monad.State
 
-
 rotateLeft = drop <> take
 
-
+ 
 type SwapListZipper a = ([a], [a], [a])
 
-createSwapListZipper :: [a] -> SwapListZipper a
-createSwapListZipper (x:xs) = ([], [x], xs)
+swapListZipper :: [a] -> SwapListZipper a
+swapListZipper (x:xs) = ([], [x], xs)
 
 nextSwap :: SwapListZipper a -> SwapListZipper a
 nextSwap (ls, [x], []) = (ls, [x], [])
@@ -19,19 +18,19 @@ nextSwap (ls, [x], rs) = (ls', [x], rs')
     rs' = drop 1 rs
 
 curList :: SwapListZipper a -> [a]
-curList (ls, x, rs) = concat[ls, x, rs]
-
+curList (ls, x, rs) = concat [ls, x, rs]
 
 headElemSwapList :: [a] -> [[a]]
 headElemSwapList xs = let (sss, _) = runSwap in sss
   where
-    runSwap = runState swapState (createSwapListZipper xs)
+    runSwap = runState swapState (swapListZipper xs)
     swapState = sequence $ replicate (length xs) nextSwapState
     
     nextSwapState :: State (SwapListZipper a) [a]
     nextSwapState = state $ \zipper -> (curList zipper, nextSwap zipper)
 
 
+-- permutations
 perms :: [a] -> [[a]]
 perms [] = [[]]
 perms (x:xs) = concat $ zipWith merge curSwapList subPermList
@@ -39,4 +38,4 @@ perms (x:xs) = concat $ zipWith merge curSwapList subPermList
     curSwapList = headElemSwapList (x:xs)
     subPermList = map (\(_:ys) -> perms ys) $ curSwapList
     
-    merge (x:xs) yss = map (x:) yss
+    merge (x:_) yss = map (x:) yss
