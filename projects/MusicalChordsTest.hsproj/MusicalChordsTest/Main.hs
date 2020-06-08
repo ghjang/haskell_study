57468e3d0e6a06557fce12fe81b5a32e75892285
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-06-08 17:12:59 +0900

    added codes for main function.

--- projects/MusicalChordsTest.hsproj/MusicalChordsTest/Main.hs
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/Main.hs
@@ -1,3 +1,27 @@
+import Control.Applicative
 import SimplePitch
+import SimpleInterval
+import SimpleScale
+import Chord
 
-main = print "Hello"
+main = mapM_ putStrLn $ zipWith (\x y -> show x
+                                           ++ (if hasFlat x || hasSharp x then "\t" else "\t\t")
+                                           ++ "Major Scale Diatonic Triads: "
+                                           ++ show y)
+                                twelvePitchF
+                                twelveMajorScaleDiatonicTriad
+  where
+    twelveMajorScaleDiatonicTriad = map majorScaleDiatonicTriad $ map (scale Major) twelvePitchF
+
+type Scale = [Pitch]
+
+majorScaleDiatonicTriad :: Scale -> [TriadChord]
+majorScaleDiatonicTriad s = getZipList $ ZipList fs <*> ZipList s
+  where
+    fs = [ majorTriad
+         , minorTriad
+         , minorTriad
+         , majorTriad
+         , majorTriad
+         , minorTriad
+         , diminishedTriad ]
