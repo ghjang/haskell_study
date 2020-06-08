9208293d75f42e7cc0533b285f084a895fbae490
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-06-08 14:54:59 +0900

    added rotateLeft function.

--- projects/MusicalChordsTest.hsproj/MusicalChordsTest/Utility.hs
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/Utility.hs
@@ -1,6 +1,12 @@
 module Utility
 ( getMaybe
+, rotateLeft
 ) where
+
+import Data.Monoid
   
 getMaybe :: Maybe a -> a
 getMaybe (Just x) = x
+
+rotateLeft :: Int -> [a] -> [a]
+rotateLeft = drop <> take
