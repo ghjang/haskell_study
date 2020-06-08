7279a57466f7f5173300ef66f81685781c5e10c5
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-06-08 14:25:14 +0900

    added Indexable type class.

---
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/Indexable.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Indexable
+( Indexable
+) where
+
+import Data.List
+
+class (Eq a, Bounded a, Enum a) => Indexable a where
+  indexOf :: a -> Int
+  indexOf e = fst . head $ dropWhile (\(x, y) -> e /= y)
+                         $ zip [0..] [(minBound :: a) .. (maxBound :: a)]
+                         
+  fromIndex :: Int -> Maybe a
+  fromIndex n = foldl f Nothing $ zip [0..] [(minBound :: a) .. (maxBound :: a)]
+    where
+      f (Just e) _ = Just e
+      f Nothing (x, y) = if n == x then Just y else Nothing
