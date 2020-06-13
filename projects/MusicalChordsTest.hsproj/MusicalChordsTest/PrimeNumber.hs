dab7c51e1c987f0658821f485dd6193da128b390
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-06-13 17:57:34 +0900

    added PrimeNumber and SinsoidalFunction modules.

---
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/PrimeNumber.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module PrimeNumber
+( primes
+) where
+  
+import Control.Monad.State
+
+primes :: [Int]
+primes = fst $ runState (sequence $ repeat nextPrime) $ [2..]
+  where
+    nextPrime = state $ \(x:xs) -> (x, filter (\y -> y `mod` x /= 0) xs)
---
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/SimpleMusic/SinusoidalFunction.hs
@@ -0,0 +1,39 @@
+module SimpleMusic.SinusoidalFunction
+( naturalHarmonics
+, evenHarmonics
+, oddHarmonics
+, squaredHarmonics
+, primeHarmonics
+) where
+
+import PrimeNumber (primes)
+
+naturalNums = [2..]
+evenNums    = [2,4..]
+oddNums     = [3,5..]
+squaredNums = map (\x -> x * x) [2..]
+
+harmonicTerm :: Float -> (Float -> Float)
+harmonicTerm m = (/ m) . sin . (m *)
+
+takeHarmonicTerm :: Int -> [Float] -> [(Float -> Float)]
+takeHarmonicTerm n ms = take n $ map harmonicTerm (1:ms)
+
+harmonicTermSum :: [Float] -> Float -> Float
+harmonicTermSum ms = sum . (sequenceA $ takeHarmonicTerm 12 ms)
+
+
+naturalHarmonics :: Float -> Float
+naturalHarmonics = harmonicTermSum naturalNums
+
+evenHarmonics :: Float -> Float
+evenHarmonics = harmonicTermSum evenNums
+
+oddHarmonics :: Float -> Float
+oddHarmonics = harmonicTermSum oddNums
+
+squaredHarmonics :: Float -> Float
+squaredHarmonics = harmonicTermSum squaredNums
+
+primeHarmonics :: Float -> Float
+primeHarmonics = harmonicTermSum (map fromIntegral primes)
