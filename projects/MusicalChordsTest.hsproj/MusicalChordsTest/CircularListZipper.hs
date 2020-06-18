0cc6ffad64cd724e598bbc8a42e3663e0385540c
Gil Ho, Jang <jmxexpert@hotmail.com>
2020-06-18 17:43:12 +0900

    fixed bugs.
    - added supports for State Monad.

--- projects/MusicalChordsTest.hsproj/MusicalChordsTest/CircularListZipper.hs
+++ projects/MusicalChordsTest.hsproj/MusicalChordsTest/CircularListZipper.hs
@@ -1,21 +1,36 @@
 module CircularListZipper
 ( CircularListZipper
+, createCircularListZipper
 , next
 , prev
+, nextItem
+, prevItem
 ) where
   
-type CircularListZipper a = (([a], [a]), ([a], [a]))
+import Control.Monad.State
+
+data Direction = Forward | Backward deriving Show
+  
+type CircularListZipper a = (([a], [a]), ([a], [a]), Maybe Direction)
 
 createCircularListZipper :: [a] -> CircularListZipper a
 createCircularListZipper [] = error "empty list is passed."
-createCircularListZipper xs = (([], []), (xs, reverse xs))
+createCircularListZipper xs = (([], []), (xs, reverse xs), Nothing)
 
 next :: CircularListZipper a -> (a, CircularListZipper a)
-next (([], []), ([], [])) = error "empty list"
-next ((xs, ys), ([], [])) = next (([], []), (ys, xs))
-next ((xs, ys), (f:fs, b:bs)) = (f, ((f:xs, b:ys), (fs, bs)))
+next (([], []), ([], []), _) = error "empty list"
+next ((xs, ys), ([], []), Just Backward) = next . snd $ next (([], []), (ys, xs), Just Forward)
+next ((xs, ys), ([], []), _) = next (([], []), (ys, xs), Just Forward)
+next ((xs, ys), (f:fs, b:bs), _) = (f, ((f:xs, b:ys), (fs, bs), Just Forward))
 
 prev :: CircularListZipper a -> (a, CircularListZipper a)
-prev (([], []), ([], [])) = error "empty list"
-prev ((xs, ys), ([], [])) = prev (([], []), (ys, xs))
-prev ((xs, ys), (f:fs, b:bs)) = (b, ((f:xs, b:ys), (fs, bs)))
+prev (([], []), ([], []), _) = error "empty list"
+prev ((xs, ys), ([], []), Just Forward) = prev . snd $ prev (([], []), (ys, xs), Just Backward)
+prev ((xs, ys), ([], []), _) = prev (([], []), (ys, xs), Just Backward)
+prev ((xs, ys), (f:fs, b:bs), _) = (b, ((f:xs, b:ys), (fs, bs), Just Backward))
+
+nextItem :: State (CircularListZipper a) a
+nextItem = state next
+
+prevItem :: State (CircularListZipper a) a
+prevItem = state prev
